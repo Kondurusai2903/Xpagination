9a2c9a39a00d36c4e350239a75dfd439d2a89ae8
kondurusai <kondurusai863@gmail.com>
2024-06-20 11:22:09 +0530

    pagination done

---
+++ xpagination/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Xpagination from "./xpagination/xpagination";
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>Employee Table Data</h1>
+      </center>
+      <Xpagination />
+    </div>
+  );
+}
+
+export default App;
---
+++ xpagination/src/xpagination/xpagination.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import "./xpagination.css";
+const Xpagination = () => {
+  const [apidata, setApidata] = useState([]);
+  const [loading, setLoading] = useState(1);
+  const API =
+    "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+  const res = async () => {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      console.log(data);
+      setApidata(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    res();
+  }, []);
+  console.log(apidata, "the data stored");
+  const handleIncrement = () => {
+    if (loading < 5) {
+      setLoading(loading + 1);
+    } else {
+      alert("you can't increment futhur");
+    }
+  };
+  const handleDecrement = () => {
+    if (loading > 1) {
+      setLoading(loading - 1);
+    } else {
+      alert("you can't decrement futhur");
+    }
+  };
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+        <tbody>
+          {apidata.map((val, ind) =>
+            ind >= loading * 10 - 10 && ind < loading * 10 ? (
+              <tr>
+                <td>{val.id}</td>
+                <td>{val.name}</td>
+                <td>{val.email}</td>
+                <td>{val.role}</td>
+              </tr>
+            ) : (
+              ""
+            )
+          )}
+        </tbody>
+      </table>
+      <center>
+        <button onClick={() => handleDecrement()}>Previous</button>
+        <button>{loading}</button>
+        <button onClick={() => handleIncrement()}>Next</button>
+      </center>
+    </div>
+  );
+};
+
+export default Xpagination;
